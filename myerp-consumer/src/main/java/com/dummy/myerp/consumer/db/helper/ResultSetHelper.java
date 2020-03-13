604fcc5bf5b8562f0e5ce99400f565dd9a5c666e
Sam <samuel.guiochon@axa.fr>
2020-03-13 15:21:46 +0100

    Added U.T.

--- myerp-consumer/src/main/java/com/dummy/myerp/consumer/db/helper/ResultSetHelper.java
+++ myerp-consumer/src/main/java/com/dummy/myerp/consumer/db/helper/ResultSetHelper.java
@@ -1,79 +1,72 @@
 package com.dummy.myerp.consumer.db.helper;
 
+import org.apache.commons.lang3.time.DateUtils;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Date;
 
-import org.apache.commons.lang3.time.DateUtils;
 
-
-/**
- * Classe utilitaire travaillant sur les ResultSet
- */
 public abstract class ResultSetHelper {
 
-    // ==================== Constructeurs ====================
-    /**
-     * Constructeur.
-     */
-    protected ResultSetHelper() {
-        super();
-    }
+	protected ResultSetHelper() {
+		super();
+	}
 
+	/**
+	 * Renvoie la valeur de la colonne pColName dans un <code>Integer</code>.
+	 * Si la colonne vaut <code>null</code>, la méthode renvoie <code>null</code>
+	 *
+	 * @param pRS      : Le ResultSet à intéroger
+	 * @param pColName : Le nom de la colonne dans le retour de la requête SQL
+	 * @return <code>Integer</code> ou <code>null</code>
+	 * @throws SQLException sur erreur SQL
+	 */
+	public static Integer getInteger(ResultSet pRS, String pColName) throws SQLException {
+		Integer vRetour = null;
+		int vInt = pRS.getInt(pColName);
+		if (!pRS.wasNull()) {
+			return vInt;
+		} else {
+			return null;
+		}
+	}
 
-    // ==================== Méthodes ====================
-    /**
-     * Renvoie la valeur de la colonne pColName dans un <code>Integer</code>.
-     * Si la colonne vaut <code>null</code>, la méthode renvoie <code>null</code>
-     *
-     * @param pRS : Le ResultSet à intéroger
-     * @param pColName : Le nom de la colonne dans le retour de la requête SQL
-     * @return <code>Integer</code> ou <code>null</code>
-     * @throws SQLException sur erreur SQL
-     */
-    public static Integer getInteger(ResultSet pRS, String pColName) throws SQLException {
-        Integer vRetour = null;
-        int vInt = pRS.getInt(pColName);
-        if (!pRS.wasNull()) {
-            vRetour = new Integer(vInt);
-        }
-        return vRetour;
-    }
+	/**
+	 * Renvoie la valeur de la colonne pColName dans un <code>Long</code>.
+	 * Si la colonne vaut <code>null</code>, la méthode renvoie <code>null</code>
+	 *
+	 * @param pRS      : Le ResultSet à intéroger
+	 * @param pColName : Le nom de la colonne dans le retour de la requête SQL
+	 * @return <code>Long</code> ou <code>null</code>
+	 * @throws SQLException sur erreur SQL
+	 */
+	public static Long getLong(ResultSet pRS, String pColName) throws SQLException {
+		Long vRetour = null;
+		Long vLong = pRS.getLong(pColName);
+		if (!pRS.wasNull()) {
+			return vLong;
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     * Renvoie la valeur de la colonne pColName dans un <code>Long</code>.
-     * Si la colonne vaut <code>null</code>, la méthode renvoie <code>null</code>
-     *
-     * @param pRS : Le ResultSet à intéroger
-     * @param pColName : Le nom de la colonne dans le retour de la requête SQL
-     * @return <code>Long</code> ou <code>null</code>
-     * @throws SQLException sur erreur SQL
-     */
-    public static Long getLong(ResultSet pRS, String pColName) throws SQLException {
-        Long vRetour = null;
-        Long vLong = pRS.getLong(pColName);
-        if (!pRS.wasNull()) {
-            vRetour = new Long(vLong);
-        }
-        return vRetour;
-    }
-
-
-    /**
-     * Renvoie la valeur de la colonne pColName dans un {@link Date} en faisant un truncate de l'heure.
-     * Si la colonne vaut <code>null</code>, la méthode renvoie <code>null</code>.
-     *
-     * @param pRS : Le ResultSet à intéroger
-     * @param pColName : Le nom de la colonne dans le retour de la requête SQL
-     * @return {@link Date} ou <code>null</code>
-     * @throws SQLException sur erreur SQL
-     */
-    public static Date getDate(ResultSet pRS, String pColName) throws SQLException {
-        Date vDate = pRS.getDate(pColName);
-        if (vDate != null) {
-            vDate = DateUtils.truncate(vDate, Calendar.DATE);
-        }
-        return vDate;
-    }
+	/**
+	 * Renvoie la valeur de la colonne pColName dans un {@link Date} en faisant un truncate de l'heure.
+	 * Si la colonne vaut <code>null</code>, la méthode renvoie <code>null</code>.
+	 *
+	 * @param pRS      : Le ResultSet à intéroger
+	 * @param pColName : Le nom de la colonne dans le retour de la requête SQL
+	 * @return {@link Date} ou <code>null</code>
+	 * @throws SQLException sur erreur SQL
+	 */
+	public static Date getDate(ResultSet pRS, String pColName) throws SQLException {
+		Date vDate = pRS.getDate(pColName);
+		if (vDate != null) {
+			return DateUtils.truncate(vDate, Calendar.DATE);
+		} else {
+			return null;
+		}
+	}
 }
---
+++ myerp-consumer/src/test/java/com/dummy/myerp/consumer/db/helper/ResultSetHelperTest.java
@@ -0,0 +1,106 @@
+package com.dummy.myerp.consumer.db.helper;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+public class ResultSetHelperTest {
+
+	@Mock
+	ResultSet resultSetMock;
+
+	@Test
+	public void testGetDateWhenNull() throws SQLException {
+		//Arrange
+		when(resultSetMock.getDate(anyString())).thenReturn(null);
+		// Act
+		Date date = ResultSetHelper.getDate(resultSetMock, "dummycolumn name");
+		// Assert
+		Assert.assertNull("Date conversion - returns null", date);
+	}
+
+	@Test
+	public void testGetDateWhenNotNull() throws SQLException, ParseException {
+		// Arrange
+		java.sql.Date simulatedDate = new java.sql.Date(System.currentTimeMillis());
+		simulatedDate.setTime(System.currentTimeMillis());
+		doReturn(simulatedDate).when(resultSetMock).getDate(anyString());
+		Calendar calendar = GregorianCalendar.getInstance();
+
+		// Act
+		Date date = ResultSetHelper.getDate(resultSetMock, "dummy column name");
+
+		// Assert
+		calendar.setTime(date);
+		Assert.assertEquals("Date conversion - no hours", 0, calendar.get(Calendar.HOUR));
+		Assert.assertEquals("Date conversion - no minutes", 0, calendar.get(Calendar.MINUTE));
+		Assert.assertEquals("Date conversion - no seconds", 0, calendar.get(Calendar.SECOND));
+	}
+
+	@Test
+	public void testGetIntegerWhenNull() throws SQLException {
+		// Arrange
+		doReturn(true).when(resultSetMock).wasNull();
+		doReturn(1000).when(resultSetMock).getInt(anyString());
+
+		// Act
+		Integer integer = ResultSetHelper.getInteger(resultSetMock, "dummy column name");
+
+		// Assert
+		Assert.assertNull("Integer conversion - returns null", integer);
+	}
+
+	@Test
+	public void testGetIntegerWhenNotNull() throws SQLException {
+		// Arrange
+		doReturn(false).when(resultSetMock).wasNull();
+		doReturn(1000).when(resultSetMock).getInt(anyString());
+
+		// Act
+		Integer integer = ResultSetHelper.getInteger(resultSetMock, "dummy column name");
+
+		// Assert
+		Assert.assertEquals("Integer conversion", Integer.valueOf(1000), integer);
+	}
+
+	@Test
+	public void testGetLongWhenNull() throws SQLException {
+		// Arrange
+		doReturn(true).when(resultSetMock).wasNull();
+		doReturn(1000L).when(resultSetMock).getLong(anyString());
+
+		// Act
+		Long longValue = ResultSetHelper.getLong(resultSetMock, "dummy column name");
+
+		// Assert
+		Assert.assertNull("Long conversion - returns null", longValue);
+	}
+
+	@Test
+	public void testGetLongWhenNotNull() throws SQLException {
+		// Arrange
+		doReturn(false).when(resultSetMock).wasNull();
+		doReturn(1000L).when(resultSetMock).getLong(anyString());
+
+		// Act
+		Long longValue = ResultSetHelper.getLong(resultSetMock, "dummy column name");
+
+		// Assert
+		Assert.assertEquals("Long conversion", Long.valueOf(1000), longValue);
+	}
+
+}
